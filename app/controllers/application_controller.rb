dc6b3523c2fb777e3388b5b8171b9dd7a5c41233
IkegzI <kegz@mail.ru>
2020-04-06 09:06:17 +0800

    add route to root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  def sign_in
-    redirect_to '/users/sign_in'
-  end
+  # def sign_in
+  #   redirect_to '/users/sign_in'
+  # end
+  binding.pry
 end
